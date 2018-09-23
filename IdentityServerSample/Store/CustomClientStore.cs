40d8fc07532a52f6a69c8966e870b53bffebd796
Obed Amoasi <obedamoasi@gmail.com>
2018-09-23 11:11:07 +0000

    implement custom stores

---
+++ IdentityServerSample/Store/CustomClientStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Stores;
+using IdentityServerSample.Interface;
+
+namespace IdentityServerSample.Store
+{
+    public class CustomClientStore : IClientStore
+    {
+        protected IRepository _dbRepository;
+
+        public CustomClientStore(IRepository repository)
+        {
+            _dbRepository = repository;
+        }
+
+        public Task<Client> FindClientByIdAsync(string clientId)
+        {
+            var client = _dbRepository.Single<Client>(c => c.ClientId == clientId);
+
+            return Task.FromResult(client);
+        }
+
+     
+    }
+}
