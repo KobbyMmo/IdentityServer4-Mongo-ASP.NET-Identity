d295adf8a9f6b784a34e28ddf6e0d887095925c3
Obed Amoasi <obedamoasi@gmail.com>
2018-09-23 10:56:06 +0000

    scafold basic asp.net identity

---
+++ IdentityServerSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityServerSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
