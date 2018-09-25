7ec9b3594ff9ddedfe041acca5aee4d302503c8e
Obed Amoasi <obedamoasi@gmail.com>
2018-09-25 11:56:52 +0000

    implement email sender interface

--- IdentityServerSample/Services/EmailSender.cs
+++ IdentityServerSample/Services/EmailSender.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +13,15 @@ namespace IdentityServerSample.Services
     {
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            return Task.CompletedTask;
+            // Plug in your email service here to send an email.
+            //var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+            var client = new SendGridClient("");
+            var from = new EmailAddress("test@example.com", "Admin");
+            var to = new EmailAddress(email, email);
+            var plainTextContent = message;
+            var htmlContent = message;
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            return client.SendEmailAsync(msg);
         }
     }
 }
