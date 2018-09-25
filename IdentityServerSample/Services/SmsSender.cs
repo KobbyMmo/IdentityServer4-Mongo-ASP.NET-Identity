ba35d1278843f03f2a7880073538312fc005d4a8
Obed Amoasi <obedamoasi@gmail.com>
2018-09-25 11:57:21 +0000

    implement sms sender interface

---
+++ IdentityServerSample/Services/SmsSender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace IdentityServerSample.Services
+{
+    public class SmsSender : ISmsSender
+    {
+        public Task SendSmsAsync(string number, string message)
+        {
+            const string accountSid = "";
+            const string authToken = "";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            return MessageResource.CreateAsync(
+                body: message,
+                from: new Twilio.Types.PhoneNumber(""),
+                to: new Twilio.Types.PhoneNumber(number)
+            );
+        }
+    }
+}
