eeae1f8232c149bab8970ac51da25d4cc67b0734
Obed Amoasi <obedamoasi@gmail.com>
2018-09-25 07:58:57 +0000

    create sms sender interface

---
+++ IdentityServerSample/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerSample.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
