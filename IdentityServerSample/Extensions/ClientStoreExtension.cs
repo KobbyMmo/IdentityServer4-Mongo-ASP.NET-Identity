26fa9c41f9dc0aec3ad0f16d4ff91fe5a37766c4
Obed Amoasi <obedamoasi@gmail.com>
2018-09-26 07:35:13 +0000

    add client store extension to check if client is configured with PKCE

---
+++ IdentityServerSample/Extensions/ClientStoreExtension.cs
@@ -0,0 +1,28 @@
+﻿using IdentityServer4.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentitySample.Mvc.Extension
+{
+    public static class ClientStoreExtension
+    {
+        /// <summary>
+        /// Determines whether the client is configured to use PKCE.
+        /// </summary>
+        /// <param name="store">The store.</param>
+        /// <param name="client_id">The client identifier.</param>
+        /// <returns></returns>
+        public static async Task<bool> IsPkceClientAsync(this IClientStore store, string client_id)
+        {
+            if (!string.IsNullOrWhiteSpace(client_id))
+            {
+                var client = await store.FindEnabledClientByIdAsync(client_id);
+                return client?.RequirePkce == true;
+            }
+
+            return false;
+        }
+    }
+}
